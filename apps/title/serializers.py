6df6cc48171f07e0fd98e1d9163a24a1aa44065b
Sekdush <candycakechocolate@hotmail.com>
2020-01-14 22:49:59 +0800

    read_only

--- apps/title/serializers.py
+++ apps/title/serializers.py
@@ -6,6 +6,16 @@ import datetime
 
 class TitleSerializer(serializers.ModelSerializer):
 
+    user = serializers.HiddenField(
+        default=serializers.CurrentUserDefault())
+
+    is_delete = serializers.DateTimeField(read_only=True)
+
+    def validate(self, attrs):
+        # 调用一个方法生成order_sn
+        attrs['is_delete'] = 0
+        return attrs
+
     class Meta:
         model = Title
         fields = '__all__'  # 序列化全部字段，实际中不建议使用，因为像password等字段是不应该返回给前端的
