6a263fd195638eb72524ec7f95e844133b730334
Skedush <candycakechocolate@hotmail.com>
2022-01-16 22:10:04 +0800

    feat: 🎸 (category)分类modal添加字段

--- apps/category/models.py
+++ apps/category/models.py
@@ -8,6 +8,17 @@ class Category(models.Model):
 
     # 字符串类型，max_length表示最大长度255，verbose_name表示备注的字段中文名
     name = models.CharField(max_length=100, verbose_name='分类名称')
+    link = models.CharField(max_length=100, null=True,
+                            blank=True, verbose_name='分类连接')
+    protocol = models.CharField(
+        max_length=100, default='https://', null=True,
+        blank=True, verbose_name='协议')
+    has_user_rule = models.BooleanField(
+        default=True, verbose_name='是否加用户标识二级域名规则')
+    child_link = models.CharField(
+        max_length=100, default='', null=True,
+        blank=True, verbose_name='子条目连接')
+    show_count = models.IntegerField(default=0, verbose_name='超出展示')
     # 时间类型 auto_now_add为添加时的时间，更新对象时不会有变动。
     created = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
     # 时间类型 auto_now无论是你添加还是修改对象，时间为你添加或者修改的时间。
@@ -17,6 +28,7 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
     class Meta:
         db_table = 'h_category'
         verbose_name = '分类表'
--- apps/category/serializers.py
+++ apps/category/serializers.py
@@ -14,7 +14,8 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         # fields = '__all__'  # 序列化全部字段，实际中不建议使用，因为像password等字段是不应该返回给前端的
-        fields = ('id', 'name', 'is_delete')  # 指定序列化的字段
+        fields = ('id', 'name', 'is_delete', 'link',
+                  'child_link', 'show_count', 'has_user_rule', 'protocol')  # 指定序列化的字段
 
 
 class CategoryEntrySerializer(serializers.ModelSerializer):
